29a667a401ce0c5b8dbac3f01672a50f18370bbc
wolderado <wolderado@gmail.com>
2021-07-05 16:04:06 +0300

    analytics js update

--- assets/js/Analytics.js
+++ assets/js/Analytics.js
@@ -1,8 +1,6 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-P4Z19PZHMX"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-P4Z19PZHMX');
-</script>
